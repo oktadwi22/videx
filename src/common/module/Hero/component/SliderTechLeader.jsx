60535112ff4f3a316d72067827082430a16baee3
oktadwi22 <129654132+oktadwi22@users.noreply.github.com>
2024-03-20 12:50:02 +0700

    Update SliderTechLeader.jsx

--- src/common/module/Hero/component/SliderTechLeader.jsx
+++ src/common/module/Hero/component/SliderTechLeader.jsx
@@ -10,7 +10,7 @@ export default function SliderTechLeader() {
       {TechIcon.map((item, index) => (
         <div className="w-[150px]" key={index}>
         <Image
-          className="w-full h-auto object-cover bg-gray-300 rounded-lg"
+          className="w-full h-auto object-cover bg-gray-300 rounded-lg p-2"
           src={item.uni}
           alt="aa"
           width={400}
