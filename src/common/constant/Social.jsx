8b7b97a817b917a42d1884594ccef5cfdb88b823
farmer <Ahmdpmbd24@gmail.com>
2024-04-05 22:33:54 +0700

    Update Social

--- src/common/constant/Social.jsx
+++ src/common/constant/Social.jsx
@@ -6,13 +6,13 @@ const size = 20
 export const SocialItem = [
     {
         logo: <FaDribbble size={size} className="group-hover:rotate-180 transition-transform duration-500 relative"  />,
-        title: 'Legal',
-        href: 'https://privago.gitbook.io/privago/legal-considerations-and-compliance'
+        title: 'Brand Kit',
+        href: 'https://www.privago.ai/brand_kit/logo_privago_ai.zip'
     },
     {
         logo: <FaDribbble size={size} className="group-hover:rotate-180 transition-transform duration-500 relative"  />,
-        title: 'Brand Kit',
-        href: 'https://www.privago.ai/brand_kit/logo_privago_ai.zip'
+        title: 'Legal',
+        href: 'https://privago.gitbook.io/privago/legal-considerations-and-compliance'
     },
     {
         logo: <FaGithub size={size} className="group-hover:rotate-180 transition-transform duration-500 relative" />,
